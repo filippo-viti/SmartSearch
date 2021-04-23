838bf4d319e961338ca13a7c41eea92fc3d74020
Filippo Viti <filippo.viti2002@gmail.com>
2021-04-23 16:05:45 +0200

    Added search results page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,22 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return $this->render('main/index.html.twig');
+    }
+
+    /**
+     * @Route("/search/{query}", name="search")
+     *
+     * @param CityRepository $repository
+     * @param string $query
+     *
+     * @return Response
+     */
+    public function showSearchResults(CityRepository $repository, string $query): Response
+    {
+        $cities = $repository->findByNameAutocomplete($query);
+        return $this->render('main/results.html.twig', [
+            'query' => $query,
+            'cities' => $cities
+        ]);
     }
 }
